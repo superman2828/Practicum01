be7e2787668939030a7d3b7a8fcfdf0106e4d728
Nolan Earl <nearl2828@gmail.com>
2026-01-15 12:40:21 -0500

    Initial commit

---
+++ src/PersonGenerator.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+
+public class PersonGenerator {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        ArrayList<String> recs = new ArrayList<>();
+
+        String id;
+        String firstName;
+        String lastName;
+        String title;
+        int yob;
+
+        String rec;
+
+        boolean done = false;
+
+        do{
+            id = SafeInput.getNonZeroLenString(in, "Enter the ID");
+            firstName = SafeInput.getNonZeroLenString(in, "Enter the First Name");
+            lastName = SafeInput.getNonZeroLenString(in, "Enter the Last Name");
+            title = SafeInput.getNonZeroLenString(in, "Enter the Title");
+            yob = SafeInput.getInt(in, "Enter the Year of Birth");
+
+            rec = id + ", " + firstName + ", " + lastName + ", " + title + ", " + yob;
+
+            // System.out.println(rec);
+
+            recs.add(rec);
+
+            done = SafeInput.getYNConfirm(in, "Are you done?");
+
+        }while(!done);
+
+        File workingDirectory = new File(System.getProperty("user.dir"));
+        Path file = Paths.get(workingDirectory.getPath() + "\\src\\PersonTestData.txt");
+
+        try
+        {
+            // Typical java pattern of inherited classes
+            // we wrap a BufferedWriter around a lower level BufferedOutputStream
+            OutputStream out =
+                    new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            BufferedWriter writer =
+                    new BufferedWriter(new OutputStreamWriter(out));
+
+            // Finally can write the file LOL!
+
+            for(String newRec : recs)
+            {
+                writer.write(newRec, 0, newRec.length());  // stupid syntax for write rec
+                // 0 is where to start (1st char) the write
+                // rec. length() is how many chars to write (all)
+                writer.newLine();  // adds the new line
+
+            }
+            writer.close(); // must close the file to seal it and flush buffer
+            System.out.println("Data file written!");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
